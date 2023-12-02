ed96bfa9902c1ef4afb8bc52453df77ec19ad65e
M Chaithanya <chaithanyamadduru03@gmail.com>
2023-12-02 15:06:40 +0530

    user can edit task

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,5 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      @task = Task.new
-      @tasks = current_user.tasks
-    else
-    end
     @public_users = User.joins(:tasks).where(privacy:false)
-
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,31 @@
 class TasksController < ApplicationController
 
+  def new
+    @task = Task.new
+  end
 
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
       redirect_to root_path, notice: 'Task was successfully created.'
     else
-      render root
+      # binding.break
+      render :new
     end
   end
+
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to root_path, notice: 'Task was successfully updates.'
+    else
+      render :edit
+    end
+  end
+
   def destroy
     task = Task.find(params[:id])
     task.destroy
